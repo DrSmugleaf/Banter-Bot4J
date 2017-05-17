b2a87b8de1609eaff30b7a8a8cb211a83018130e
DrSmugleaf <javier.frayluis@gmail.com>
2017-05-17 23:54:55 +0200

    Added checks for blacklisting users with higher roles, the owner or yourself

--- src/main/java/com/github/drsmugbrain/commands/Admin.java
+++ src/main/java/com/github/drsmugbrain/commands/Admin.java
@@ -2,10 +2,12 @@ package com.github.drsmugbrain.commands;
 
 import com.github.drsmugbrain.models.Member;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.Permissions;
 import sx.blah.discord.util.RequestBuffer;
 
+import java.util.Comparator;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -15,11 +17,16 @@ import java.util.List;
 public class Admin {
 
     public static void blacklist(MessageReceivedEvent event, List<String> args) {
-        EnumSet<Permissions> permissions = event.getAuthor().getPermissionsForGuild(event.getGuild());
+        IUser author = event.getAuthor();
+        EnumSet<Permissions> permissions = author.getPermissionsForGuild(event.getGuild());
 
         if(permissions.contains(Permissions.KICK) || permissions.contains(Permissions.BAN)) {
             Long guildID = event.getGuild().getLongID();
             List<IUser> mentions = event.getMessage().getMentions();
+
+            List<IRole> authorRoles = author.getRolesForGuild(event.getGuild());
+            authorRoles.sort(Comparator.comparingInt(IRole::getPosition));
+            IRole highestAuthorRole = authorRoles.get(authorRoles.size() - 1);
 
             mentions.forEach(mention -> {
                 Member member = Member.get(mention.getLongID(), guildID);
@@ -27,6 +34,26 @@ public class Admin {
 
                 if(member == null) {
                     RequestBuffer.request(() -> event.getChannel().sendMessage("User " + nickname + " doesn't exist"));
+                    return;
+                }
+
+                if(author.getLongID() == mention.getLongID()) {
+                    RequestBuffer.request(() -> event.getChannel().sendMessage("You can't blacklist yourself!"));
+                    return;
+                }
+
+                if(mention.getLongID() == event.getGuild().getOwner().getLongID()) {
+                    RequestBuffer.request(() -> event.getChannel().sendMessage("You can't blacklist the server owner!"));
+                    return;
+                }
+
+                List<IRole> mentionRoles = mention.getRolesForGuild(event.getGuild());
+                mentionRoles.sort(Comparator.comparingInt(IRole::getPosition));
+                IRole highestMentionRole = mentionRoles.get(mentionRoles.size() - 1);
+
+                if(highestAuthorRole.getPosition() < highestMentionRole.getPosition()) {
+                    RequestBuffer.request(() -> event.getChannel().sendMessage("You can't blacklist " + nickname + ".\n" +
+                            "Your highest role has a lower position in the role manager than their highest role."));
                     return;
                 }
 
