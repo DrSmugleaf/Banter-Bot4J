721196bc22f2178350c9172ca2f99424c28c1b76
DrSmugleaf <javier.frayluis@gmail.com>
2017-06-10 20:06:56 +0200

    Added battle turn order set

--- src/main/java/com/github/drsmugbrain/pokemon/Battle.java
+++ src/main/java/com/github/drsmugbrain/pokemon/Battle.java
@@ -1,6 +1,8 @@
 package com.github.drsmugbrain.pokemon;
 
 import javax.annotation.Nonnull;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * Created by DrSmugleaf on 07/06/2017.
@@ -8,9 +10,18 @@ import javax.annotation.Nonnull;
 public class Battle {
 
     private final Trainer TRAINERS[];
+    private Set<Pokemon> turnOrder = new LinkedHashSet<>();
 
     public Battle(@Nonnull Trainer... trainers) {
         this.TRAINERS = trainers;
+    }
+
+    public Pokemon getFirstPokemon() {
+        return turnOrder.iterator().next();
+    }
+
+    public Pokemon getLastPokemon() {
+        return (Pokemon) turnOrder.toArray()[turnOrder.size() - 1];
     }
 
 }
