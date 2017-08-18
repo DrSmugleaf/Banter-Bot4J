7c6306f9f4863c0710da7457e537ef69f4d95be0
DrSmugleaf <javier.frayluis@gmail.com>
2017-08-18 14:08:00 +0200

    Added method to print pokemons as enums from a smogon json

--- src/main/java/com/github/drsmugbrain/pokemon/SmogonParser.java
+++ src/main/java/com/github/drsmugbrain/pokemon/SmogonParser.java
@@ -61,6 +61,20 @@ public class SmogonParser {
         }
     }
 
+    private static void printPokemonsAsEnums(JSONArray pokemons) {
+        for (int i = 0; i < pokemons.length(); i++) {
+            String name = pokemons.getJSONObject(i).getString("name");
+
+            System.out.print(name
+                    .replace(" ", "_")
+                    .replace("-", "_")
+                    .replace("'", "")
+                    .toUpperCase()
+            );
+            System.out.println("(\"" + name + "\"),");
+        }
+    }
+
     private static void printAbilitiesAsEnums(JSONArray abilities) {
         for (int i = 0; i < abilities.length(); i++) {
             String name = abilities.getJSONObject(i).getString("name");
