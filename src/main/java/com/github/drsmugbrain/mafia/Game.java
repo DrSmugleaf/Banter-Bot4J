aa8e3c10d01372b5af815108dbb3eb241e38cec6
DrSmugleaf <javier.frayluis@gmail.com>
2017-08-28 12:40:38 +0200

    Mafia: Added methods to add bots to Game
    Fixed game cycle
    Added method to get human players and shuffled players display

--- src/main/java/com/github/drsmugbrain/mafia/Game.java
+++ src/main/java/com/github/drsmugbrain/mafia/Game.java
@@ -6,9 +6,7 @@ import com.github.drsmugbrain.mafia.roles.Role;
 import com.github.drsmugbrain.mafia.roles.Roles;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -17,23 +15,50 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Game {
 
     private final Setup SETUP;
-    private final Map<Long, Player> PLAYERS;
-    private Cycle CYCLE = new Cycle(this, Phase.DAY);
+    private final Map<Long, Player> PLAYERS = new HashMap<>();
+    private final List<Player> PLAYERS_DISPLAY = new ArrayList<>();
+    private final Cycle CYCLE;
+    private boolean started = false;
 
     public Game(@Nonnull Setup setup, @Nonnull Map<Long, Player> players) {
         this.SETUP = setup;
-        this.PLAYERS = players;
+        this.PLAYERS.putAll(players);
+        this.CYCLE = new Cycle(this, Phase.DAY);
     }
 
+    @Nonnull
     public Setup getSetup() {
         return this.SETUP;
     }
 
+    @Nonnull
     public Map<Long, Player> getPlayers() {
-        return this.PLAYERS;
+        return new HashMap<>(this.PLAYERS);
+    }
+
+    @Nonnull
+    public Map<Long, Player> getHumanPlayers() {
+        Map<Long, Player> players = new HashMap<>(this.PLAYERS);
+        players.entrySet().removeIf(entry -> entry.getValue().isBot());
+        return players;
+    }
+
+    @Nonnull
+    public List<Player> getPlayersDisplay() {
+        return new ArrayList<>(this.PLAYERS_DISPLAY);
+    }
+
+    @Nonnull
+    public Cycle getCycle() {
+        return this.CYCLE;
     }
 
     public void start() {
+        if (this.started) {
+            return;
+        }
+        this.started = true;
+
         List<Player> playersWithoutRoles = new ArrayList<>(this.PLAYERS.values());
 
         for (Roles role : this.SETUP.getRoles()) {
@@ -43,13 +68,29 @@ public class Game {
             randomPlayer.setRole(new Role(role));
         }
 
+        this.PLAYERS_DISPLAY.addAll(this.PLAYERS.values());
+        Collections.shuffle(this.PLAYERS_DISPLAY);
+
         EventDispatcher.dispatch(new GameStartEvent(this));
 
         this.CYCLE.resume();
     }
 
-    public Cycle getCycle() {
-        return this.CYCLE;
+    protected long getNextBotID() {
+        long lowestPlayerID = Collections.min(this.PLAYERS.keySet());
+        if (lowestPlayerID < 0) {
+            return lowestPlayerID - 1;
+        }
+        return -1;
+    }
+
+    public void fillWithBots() {
+        for (int i = this.PLAYERS.size(); i < 15; i++) {
+            long botID = this.getNextBotID();
+            Player player = new Player(botID, "Bot" + botID);
+            player.setBot(true);
+            this.PLAYERS.put(botID, player);
+        }
     }
 
 }
