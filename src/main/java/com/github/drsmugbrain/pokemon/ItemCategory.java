889aa8e17d7d88a6e8c3d4ea14a91433335fceac
DrSmugleaf <javier.frayluis@gmail.com>
2017-09-18 05:45:40 +0200

    Added item caegories

---
+++ src/main/java/com/github/drsmugbrain/pokemon/ItemCategory.java
@@ -0,0 +1,29 @@
+package com.github.drsmugbrain.pokemon;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by DrSmugleaf on 18/09/2017.
+ */
+public enum ItemCategory {
+
+    DEFAULT("Default"),
+    MEGA_STONE("Mega Stone"),
+    PRIMAL_ORB("Primal Orb"),
+    Z_CRYSTAL("Z-Crystal"),
+    ARCEUS_PLATE("Arceus Plate"),
+    GENESECT_DRIVE("Genesect Drive"),
+    SILVALLY_MEMORY("Silvally Memory");
+
+    private final String NAME;
+
+    ItemCategory(@Nonnull String name) {
+        this.NAME = name;
+    }
+
+    @Nonnull
+    public String getName() {
+        return this.NAME;
+    }
+
+}
