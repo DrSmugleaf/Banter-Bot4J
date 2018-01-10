bb23aa12ec50f56ae900ffd6e27b8e2d6ae5207d
DrSmugleaf <javier.frayluis@gmail.com>
2018-01-10 01:09:36 +0100

    Changed Env map to an instance of Properties

--- src/main/java/com/github/drsmugleaf/Env.java
+++ src/main/java/com/github/drsmugleaf/Env.java
@@ -2,11 +2,11 @@ package com.github.drsmugleaf;
 
 import com.github.drsmugleaf.util.Bot;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -14,23 +14,25 @@ import java.util.Properties;
  */
 public class Env {
 
-    public static Map<String, String> env = new HashMap<>(System.getenv());
+    @Nonnull
+    private static final Properties env;
 
     static {
+        Properties defaultProperties = new Properties();
+        System.getenv().forEach(defaultProperties::setProperty);
+        env = new Properties(defaultProperties);
+
         try(InputStream input = new FileInputStream(".env")) {
-            Properties properties = new Properties();
-            properties.load(input);
-            for (String s : properties.stringPropertyNames()) {
-                env.put(s, properties.getProperty(s));
-            }
+            env.load(input);
             input.close();
         } catch (IOException e) {
             Bot.LOGGER.error("Error reading .env file", e);
         }
     }
 
-    public static String get(String key) {
-        return env.get(key);
+    @Nullable
+    public static String get(@Nonnull String key) {
+        return env.getProperty(key);
     }
 
 }
