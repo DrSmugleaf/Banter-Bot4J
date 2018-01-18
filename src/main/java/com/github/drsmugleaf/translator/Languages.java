a3b65db9a1551f7c527789a1b38b2682b41b8a63
DrSmugleaf <javier.frayluis@gmail.com>
2018-01-18 21:27:50 +0100

    Added translator/Languages enum

---
+++ src/main/java/com/github/drsmugleaf/translator/Languages.java
@@ -0,0 +1,92 @@
+package com.github.drsmugleaf.translator;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by DrSmugleaf on 18/01/2018.
+ */
+public enum Languages {
+
+    AFRIKAANS("Afrikaans", "af"),
+    ARABIC("Arabic", "ar"),
+    BANGLA("Bangla", "bn"),
+    BOSNIAN_LATIN("Bosnian (Latin)", "bs"),
+    BULGARIAN("Bulgarian", "bg"),
+    CANTONESE_TRADITIONAL("Cantonese (Traditional)", "yue"),
+    CATALAN("Catalan", "ca"),
+    CHINESE_SIMPLIFIED("Chinese Simplified", "zh-Hans"),
+    CHINESE_TRADITIONAL("Chinese Traditional", "zh-Hant"),
+    CROATIAN("Croatian", "hr"),
+    CZECH("Czech", "cs"),
+    DANISH("Danish", "da"),
+    DUTCH("Dutch", "nl"),
+    ENGLISH("English", "en"),
+    ESTONIAN("Estonian", "et"),
+    FIJIAN("Fijian", "fj"),
+    FILIPINO("Filipino", "fil"),
+    FINNISH("Finnish", "fi"),
+    FRENCH("French", "fr"),
+    GERMAN("German", "de"),
+    GREEK("Greek", "el"),
+    HAITIAN_CREOLE("Haitian Creole", "ht"),
+    HEBREW("Hebrew", "he"),
+    HINDI("Hindi", "hi"),
+    HMONG_DAW("Hmong Daw", "mww"),
+    HUNGARIAN("Hungarian", "hu"),
+    INDONESIAN("Indonesian", "id"),
+    ITALIAN("Italian", "it"),
+    JAPANESE("Japanese", "ja"),
+    KISWAHILI("Kiswahili", "sw"),
+    KLINGON("Klingon", "tlh"),
+    KLINGON_PLQAD("Klingon (plqaD)", "tlh-Qaak"),
+    KOREAN("Korean", "ko"),
+    LATVIAN("Latvian", "lv"),
+    LITHUANIAN("Lithuanian", "lt"),
+    MALAGASY("Malagasy", "mg"),
+    MALAY("Malay", "ms"),
+    MALTESE("Maltese", "mt"),
+    NORWEGIAN("Norwegian", "nb"),
+    PERSIAN("Persian", "fa"),
+    POLISH("Polish", "pl"),
+    PORTUGUESE("Portuguese", "pt"),
+    QUERETARO_OTOMI("Queretaro Otomi", "otq"),
+    ROMANIAN("Romanian", "ro"),
+    RUSSIAN("Russian", "ru"),
+    SAMOAN("Samoan", "sm"),
+    SERBIAN_CYRILLIC("Serbian (Cyrillic)", "sr-Cyrl"),
+    SERBIAN_LATIN("Serbian (Latin)", "sr-Latn"),
+    SLOVAK("Slovak", "sk"),
+    SLOVENIAN("Slovenian", "sl"),
+    SPANISH("Spanish", "es"),
+    SWEDISH("Swedish", "sv"),
+    TAHITIAN("Tahitian", "ty"),
+    TAMIL("Tamil", "ta"),
+    THAI("Thai", "th"),
+    TONGAN("Tongan", "to"),
+    TURKISH("Turkish", "tr"),
+    UKRAINIAN("Ukrainian", "uk"),
+    URDU("Urdu", "ur"),
+    VIETNAMESE("Vietnamese", "vi"),
+    WELSH("Welsh", "cy"),
+    YUCATEC_MAYA("Yucatec Maya", "yua");
+
+    @Nonnull
+    private final String LANGUAGE;
+
+    @Nonnull
+    private final String CODE;
+
+    Languages(@Nonnull String language, @Nonnull String code) {
+        LANGUAGE = language;
+        CODE = code;
+    }
+
+    public String getLanguage() {
+        return LANGUAGE;
+    }
+
+    public String getCode() {
+        return CODE;
+    }
+
+}
