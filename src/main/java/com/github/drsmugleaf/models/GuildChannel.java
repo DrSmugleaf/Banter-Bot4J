eeb5588d72582d8157f071d0fd3a012556bd0cc0
DrSmugleaf <javier.frayluis@gmail.com>
2018-01-19 00:40:37 +0100

    Added bridged_to column to guild channel database model

--- src/main/java/com/github/drsmugleaf/models/GuildChannel.java
+++ src/main/java/com/github/drsmugleaf/models/GuildChannel.java
@@ -6,10 +6,12 @@ import sx.blah.discord.handle.impl.events.ReadyEvent;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import javax.annotation.Nullable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by DrSmugleaf on 18/01/2018.
@@ -19,10 +21,18 @@ public class GuildChannel {
     private static Connection connection;
     private long channelID;
     private long guildID;
+    public List<Long> bridgedTo = new ArrayList<>();
 
-    public GuildChannel(long channelID, long guildID) {
+    public GuildChannel(long channelID, long guildID, @Nullable List<Long> bridgedTo) {
         this.channelID = channelID;
         this.guildID = guildID;
+        if (bridgedTo != null) {
+            this.bridgedTo.addAll(bridgedTo);
+        }
+    }
+
+    public GuildChannel(long channelID, long guildID) {
+        this(channelID, guildID, null);
     }
 
     public static void createTable(Connection connection) {
@@ -32,6 +42,7 @@ public class GuildChannel {
                     "CREATE TABLE IF NOT EXISTS guild_channels (" +
                     "channel_id BIGINT REFERENCES channels (id) ON UPDATE CASCADE ON DELETE CASCADE," +
                     "guild_id BIGINT REFERENCES guilds (id) ON UPDATE CASCADE ON DELETE CASCADE," +
+                    "bridged_to BIGINT[] NOT NULL DEFAULT ARRAY[]::bigint[]," +
                     "CONSTRAINT channel_guild_pkey PRIMARY KEY (channel_id, guild_id)" +
                     ")"
             );
@@ -52,15 +63,18 @@ public class GuildChannel {
         PreparedStatement statement;
         try {
             statement = connection.prepareStatement(
-                    "INSERT INTO guild_channels (channel_id, guild_id) " +
-                    "VALUES(?, ?) " +
+                    "INSERT INTO guild_channels (channel_id, guild_id, bridged_to) " +
+                    "VALUES(?, ?, ?) " +
                     "ON CONFLICT DO NOTHING"
             );
             statement.setLong(1, channelID);
             statement.setLong(2, guildID);
+            Array array = connection.createArrayOf("bigint", bridgedTo.toArray());
+            statement.setArray(3, array);
             statement.executeUpdate();
         } catch (SQLException e) {
-            BanterBot4J.LOGGER.error("Error creating guild channel with channel id " + channelID + " and guild id " + guildID, e);
+            String bridgedToString = bridgedTo.stream().map(Object::toString).collect(Collectors.joining(", "));
+            BanterBot4J.LOGGER.error("Error creating guild channel with channel id " + channelID + " and guild id " + guildID + " with bridged to set to " + bridgedToString, e);
         }
     }
 
@@ -75,7 +89,9 @@ public class GuildChannel {
             result.next();
             channelID = result.getLong("channel_id");
             long guildID = result.getLong("guild_id");
-            return new GuildChannel(channelID, guildID);
+            Long[] array = (Long[]) result.getArray("bridged_to").getArray();
+            List<Long> bridgedTo = Arrays.asList(array);
+            return new GuildChannel(channelID, guildID, bridgedTo);
         } catch (SQLException e) {
             BanterBot4J.LOGGER.error("Error retrieving guild channel with channel id " + channelID, e);
             return null;
@@ -86,22 +102,27 @@ public class GuildChannel {
         PreparedStatement statement;
         try {
             statement = connection.prepareStatement(
-                    "INSERT INTO guild_channels (channel_id, guild_id) " +
-                    "VALUES(?, ?) " +
+                    "INSERT INTO guild_channels (channel_id, guild_id, bridged_to) " +
+                    "VALUES(?, ?, ?) " +
                     "ON CONFLICT (channel_id, guild_id) DO UPDATE " +
                     "SET " +
                     "channel_id = ?, " +
                     "guild_id = ?, " +
+                    "bridged_to = ? " +
                     "WHERE guild_channels.channel_id = ? AND guild_channels.guild_id = ?"
             );
             statement.setLong(1, channelID);
             statement.setLong(2, guildID);
-            statement.setLong(3, channelID);
-            statement.setLong(4, guildID);
-            statement.setLong(5, channelID);
-            statement.setLong(6, guildID);
+            Array array = connection.createArrayOf("BIGINT", bridgedTo.toArray());
+            statement.setArray(3, array);
+            statement.setLong(4, channelID);
+            statement.setLong(5, guildID);
+            statement.setArray(6, array);
+            statement.setLong(7, channelID);
+            statement.setLong(8, guildID);
         } catch (SQLException e) {
-            BanterBot4J.LOGGER.error("Error saving guild channel with channel id " + channelID + " and guild id " + guildID, e);
+            String bridgedToString = bridgedTo.stream().map(Object::toString).collect(Collectors.joining(", "));
+            BanterBot4J.LOGGER.error("Error saving guild channel with channel id " + channelID + " and guild id " + guildID + " with bridged to set to " + bridgedToString, e);
         }
     }
 
