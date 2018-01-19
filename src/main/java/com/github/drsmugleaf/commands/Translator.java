1ea1a20f0286610eb5446dcee659d746cb90042a
DrSmugleaf <javier.frayluis@gmail.com>
2018-01-19 13:28:38 +0100

    Added responses to bridge and unbridge commands

--- src/main/java/com/github/drsmugleaf/commands/Translator.java
+++ src/main/java/com/github/drsmugleaf/commands/Translator.java
@@ -15,18 +15,18 @@ import java.util.List;
 /**
  * Created by DrSmugleaf on 18/01/2018.
  */
-public class Translator {
+public class Translator extends AbstractCommand {
 
     @Command(permissions = {Permissions.MANAGE_CHANNELS}, tags = {Tags.GUILD_ONLY})
     public static void bridge(@Nonnull MessageReceivedEvent event, List<String> args) {
         IGuild guild = event.getGuild();
-        long firstChannelID = guild.getChannelsByName(args.get(0)).get(0).getLongID();
+        IChannel firstChannel = guild.getChannelsByName(args.get(0)).get(0);
         Languages firstLanguage = Languages.getLanguage(args.get(1));
-        long secondChannelID = guild.getChannelsByName(args.get(2)).get(0).getLongID();
+        IChannel secondChannel = guild.getChannelsByName(args.get(2)).get(0);
         Languages secondLanguage = Languages.getLanguage(args.get(3));
 
-        BridgedChannel firstBridgedChannel = new BridgedChannel(firstChannelID, secondChannelID);
-        BridgedChannel secondBridgedChannel = new BridgedChannel(secondChannelID, firstChannelID);
+        BridgedChannel firstBridgedChannel = new BridgedChannel(firstChannel.getLongID(), secondChannel.getLongID());
+        BridgedChannel secondBridgedChannel = new BridgedChannel(secondChannel.getLongID(), firstChannel.getLongID());
 
         firstBridgedChannel.channelLanguage = firstLanguage;
         firstBridgedChannel.bridgedLanguage = secondLanguage;
@@ -36,11 +36,21 @@ public class Translator {
 
         firstBridgedChannel.save();
         secondBridgedChannel.save();
+
+        sendMessage(event.getChannel(),
+                "Bridged together channels " + firstChannel.getName() +
+                " with language " + firstLanguage.getName() +
+                " and " + secondChannel.getName() +
+                " with language " + secondLanguage.getName()
+        );
     }
 
     @Command(permissions = {Permissions.MANAGE_CHANNELS}, tags = {Tags.GUILD_ONLY})
     public static void unbridge(@Nonnull MessageReceivedEvent event, List<String> args) {
-        BridgedChannel.delete(Long.parseLong(args.get(0)));
+        IChannel channel = event.getGuild().getChannelsByName(args.get(0)).get(0);
+        BridgedChannel.delete(channel.getLongID());
+
+        sendMessage(event.getChannel(), "Unbridged all channels bridged with " + channel.getName());
     }
 
     @EventSubscriber
