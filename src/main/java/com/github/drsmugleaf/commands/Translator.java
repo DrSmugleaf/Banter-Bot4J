3b3334f20158223fb34b00cd206270c8364a4621
DrSmugleaf <javier.frayluis@gmail.com>
2018-01-19 13:08:49 +0100

    Added bridge command
    Format: channel1 language1 channel2 language2

--- src/main/java/com/github/drsmugleaf/commands/Translator.java
+++ src/main/java/com/github/drsmugleaf/commands/Translator.java
@@ -1,7 +1,10 @@
 package com.github.drsmugleaf.commands;
 
+import com.github.drsmugleaf.models.BridgedChannel;
+import com.github.drsmugleaf.translator.Languages;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.Permissions;
 
 import javax.annotation.Nonnull;
@@ -13,7 +16,25 @@ import java.util.List;
 public class Translator {
 
     @Command(permissions = {Permissions.MANAGE_CHANNELS}, tags = {Tags.GUILD_ONLY})
-    public static void bridge(@Nonnull MessageReceivedEvent event, List<String> args) {}
+    public static void bridge(@Nonnull MessageReceivedEvent event, List<String> args) {
+        IGuild guild = event.getGuild();
+        long firstChannelID = guild.getChannelsByName(args.get(0)).get(0).getLongID();
+        Languages firstLanguage = Languages.getLanguage(args.get(1));
+        long secondChannelID = guild.getChannelsByName(args.get(2)).get(0).getLongID();
+        Languages secondLanguage = Languages.getLanguage(args.get(3));
+
+        BridgedChannel firstBridgedChannel = new BridgedChannel(firstChannelID, secondChannelID);
+        BridgedChannel secondBridgedChannel = new BridgedChannel(secondChannelID, firstChannelID);
+
+        firstBridgedChannel.channelLanguage = firstLanguage;
+        firstBridgedChannel.bridgedLanguage = secondLanguage;
+
+        secondBridgedChannel.channelLanguage = secondLanguage;
+        secondBridgedChannel.bridgedLanguage = firstLanguage;
+
+        firstBridgedChannel.save();
+        secondBridgedChannel.save();
+    }
 
     @EventSubscriber
     public static void handle(@Nonnull MessageReceivedEvent event) {}
