cfabb09e93f145a51dab075cab2b79796c8ce3a8
DrSmugleaf <javier.frayluis@gmail.com>
2018-02-09 14:12:14 +0100

    Added functional groups enum

---
+++ src/main/java/com/github/drsmugleaf/chemistry/organic/Groups.java
@@ -0,0 +1,22 @@
+package com.github.drsmugleaf.chemistry.organic;
+
+/**
+ * Created by DrSmugleaf on 09/02/2018.
+ */
+public enum Groups {
+
+    ALKANE,
+    ALKENE,
+    ALKYNE,
+    ALCOHOL,
+    HALOGEN,
+    KETONE,
+    ALDEHYDE,
+    CARBOXYLIC_ACID,
+    ETHER,
+    ESTER,
+    AMINE,
+    AMIDE,
+    CYCLIC_COMPOUNDS
+
+}
