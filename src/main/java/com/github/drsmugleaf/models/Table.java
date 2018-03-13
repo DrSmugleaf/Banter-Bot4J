ae457e0bf09052316514cf8335d694ad6cc266a9
DrSmugleaf <javier.frayluis@gmail.com>
2018-03-13 15:55:12 +0100

    Created table annotation

---
+++ src/main/java/com/github/drsmugleaf/models/Table.java
@@ -0,0 +1,17 @@
+package com.github.drsmugleaf.models;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by DrSmugleaf on 13/03/2018.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Table {
+
+    String name();
+
+}
