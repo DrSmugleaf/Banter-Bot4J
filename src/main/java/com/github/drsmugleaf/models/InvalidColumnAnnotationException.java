61d07d6d49fce00c9c01ca5ab38facdd3a576d0c
DrSmugleaf <javier.frayluis@gmail.com>
2018-03-31 17:23:56 +0200

    Created InvalidColumnAnnotationException class

---
+++ src/main/java/com/github/drsmugleaf/models/InvalidColumnAnnotationException.java
@@ -0,0 +1,24 @@
+package com.github.drsmugleaf.models;
+
+/**
+ * Created by DrSmugleaf on 31/03/2018.
+ */
+public class InvalidColumnAnnotationException extends Exception {
+
+    public InvalidColumnAnnotationException() {
+        super();
+    }
+
+    public InvalidColumnAnnotationException(String message) {
+        super(message);
+    }
+
+    public InvalidColumnAnnotationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidColumnAnnotationException(Throwable cause) {
+        super(cause);
+    }
+
+}
