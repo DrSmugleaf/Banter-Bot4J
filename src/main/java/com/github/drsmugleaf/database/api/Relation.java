943405d6fb8dcc208be822c1637bcab4e9b9a128
DrSmugleaf <javier.frayluis@gmail.com>
2018-04-08 23:26:55 +0200

    Created Relation annotation and Relations enum

---
+++ src/main/java/com/github/drsmugleaf/database/api/Relation.java
@@ -0,0 +1,10 @@
+package com.github.drsmugleaf.database.api;
+
+/**
+ * Created by DrSmugleaf on 08/04/2018.
+ */
+public @interface Relation {
+
+    Relations relation();
+
+}
---
+++ src/main/java/com/github/drsmugleaf/database/api/Relations.java
@@ -0,0 +1,13 @@
+package com.github.drsmugleaf.database.api;
+
+/**
+ * Created by DrSmugleaf on 08/04/2018.
+ */
+public enum Relations {
+
+    OneToOne,
+    OneToMany,
+    ManyToOne,
+    ManyToMany
+
+}
