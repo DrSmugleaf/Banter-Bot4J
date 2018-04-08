b38a1b9960b748bfe5ed596683c2c5bc62f01a26
DrSmugleaf <javier.frayluis@gmail.com>
2018-04-08 20:47:22 +0200

    Created Model.createIfNotExists method

--- src/main/java/com/github/drsmugleaf/database/api/Model.java
+++ src/main/java/com/github/drsmugleaf/database/api/Model.java
@@ -98,6 +98,55 @@ public abstract class Model {
         return models;
     }
 
+    public <T extends Model> void createIfNotExists(@Nonnull T model) throws SQLException {
+        StringBuilder query = new StringBuilder();
+        StringBuilder queryInsert = new StringBuilder();
+        StringBuilder queryValues = new StringBuilder();
+        StringBuilder queryConflict = new StringBuilder();
+
+        queryInsert
+                .append("INSERT INTO ")
+                .append(escape(getTableName(model.getClass())))
+                .append(" (");
+        queryValues.append("VALUES(");
+        queryConflict.append("ON CONFLICT DO NOTHING");
+
+        Set<Map.Entry<Field, Object>> columns = getColumns(model.getClass());
+        Iterator<Map.Entry<Field, Object>> iterator = columns.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Field, Object> entry = iterator.next();
+            Field field = entry.getKey();
+            String columnName = getColumnName(field);
+
+            queryInsert.append(columnName);
+            queryValues.append("?");
+
+            if (iterator.hasNext()) {
+                queryInsert.append(", ");
+                queryValues.append(", ");
+            } else {
+                queryInsert.append(") ");
+                queryValues.append(") ");
+            }
+        }
+
+        query
+                .append(queryInsert)
+                .append(queryValues)
+                .append(queryConflict);
+
+        PreparedStatement statement = Database.CONNECTION.prepareStatement(query.toString());
+        iterator = columns.iterator();
+        int i = 1;
+        while (iterator.hasNext()) {
+            Map.Entry<Field, Object> entry = iterator.next();
+            Object value = entry.getValue();
+            statement.setObject(i, value);
+        }
+
+        statement.executeUpdate();
+    }
+
     public <T extends Model> void save(@Nonnull T model) throws SQLException {
         StringBuilder query = new StringBuilder();
         StringBuilder queryInsert = new StringBuilder();
