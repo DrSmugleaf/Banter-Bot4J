2d33b2e96ae0a1db4675868ba15d26e31cb47416
DrSmugleaf <javier.frayluis@gmail.com>
2018-04-09 13:12:17 +0200

    Added Retention and Target annotations to Relation annotation

--- src/main/java/com/github/drsmugleaf/database/api/Relation.java
+++ src/main/java/com/github/drsmugleaf/database/api/Relation.java
@@ -1,8 +1,15 @@
 package com.github.drsmugleaf.database.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Created by DrSmugleaf on 08/04/2018.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Relation {
 
     RelationTypes type();
