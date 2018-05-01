d3702fb6e3d8e9fc256a99e77859f94f3795770c
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-01 13:37:48 +0200

    Created Status enum

---
+++ src/main/java/com/github/drsmugleaf/blackjack/Status.java
@@ -0,0 +1,15 @@
+package com.github.drsmugleaf.blackjack;
+
+/**
+ * Created by DrSmugleaf on 01/05/2018.
+ */
+public enum Status {
+
+    BLACKJACK,
+    LOST,
+    SURRENDER,
+    TIE,
+    WAITING,
+    WON
+
+}
