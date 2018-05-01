ac0a79448d85959357f6572a8f575ce42d061528
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-01 17:52:00 +0200

    Created API method to get api json response and exception class

--- src/main/java/com/github/drsmugleaf/eve/api/API.java
+++ src/main/java/com/github/drsmugleaf/eve/api/API.java
@@ -1,10 +1,46 @@
 package com.github.drsmugleaf.eve.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by DrSmugleaf on 01/05/2018.
  */
 class API {
 
-    static final String URL = "https://esi.tech.ccp.is/latest/";
+    private static final String URL = "https://esi.tech.ccp.is/latest/";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(API.class);
+
+    static String getResponse(@Nonnull String endpoint) {
+        URL url;
+        try {
+            url = new URL(URL + endpoint);
+        } catch (MalformedURLException e) {
+            throw new APIException("Error creating url instance", e);
+        }
+
+        HttpsURLConnection connection;
+        try {
+            connection = (HttpsURLConnection) url.openConnection();
+        } catch (IOException e) {
+            throw new APIException("Error opening connection to API endpoint" + endpoint, e);
+        }
+
+        String response;
+        try {
+            response = connection.getResponseMessage();
+        } catch (IOException e) {
+            throw new APIException("Error getting response message from connection to API endpoint" + endpoint, e)
+        }
+
+        return response;
+    }
 
 }
