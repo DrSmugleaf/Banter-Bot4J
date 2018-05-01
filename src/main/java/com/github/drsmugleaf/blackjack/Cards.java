475c344b1991c3cd351819508c5ed10e9f9f0331
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-01 12:06:01 +0200

    Created Blackjack Cards enum

---
+++ src/main/java/com/github/drsmugleaf/blackjack/Cards.java
@@ -0,0 +1,33 @@
+package com.github.drsmugleaf.blackjack;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by DrSmugleaf on 01/05/2018.
+ */
+public enum Cards {
+
+    ACE("Ace", 11),
+    ONE("One", 1),
+    TWO("Two", 2),
+    THREE("Three", 3),
+    FOUR("Four", 4),
+    FIVE("Five", 5),
+    SIX("Six", 6),
+    SEVEN("Seven", 7),
+    EIGHT("Eight", 8),
+    NINE("Nine", 9),
+    TEN("Ten", 10),
+    JACK("Jack", 10),
+    QUEEN("Queen", 10),
+    KING("King", 10);
+
+    public final String NAME;
+    public final int VALUE;
+
+    Cards(@Nonnull String name, int value) {
+        NAME = name;
+        VALUE = value;
+    }
+
+}
