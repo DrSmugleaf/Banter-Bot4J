85661b413b9cc4820ef46a4b8afd00472149c625
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-01 13:37:03 +0200

    Created Actions enum

---
+++ src/main/java/com/github/drsmugleaf/blackjack/Actions.java
@@ -0,0 +1,14 @@
+package com.github.drsmugleaf.blackjack;
+
+/**
+ * Created by DrSmugleaf on 01/05/2018.
+ */
+public enum Actions {
+
+    HIT,
+    STAND,
+    DOUBLE_DOWN,
+    SPLIT,
+    SURRENDER
+
+}
