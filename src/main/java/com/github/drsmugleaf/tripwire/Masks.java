c87ce4bb903a8aeda3dc12e556faaf613c0e47b7
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-16 21:14:58 +0200

    Created masks enum and added pandemic horde mask id

---
+++ src/main/java/com/github/drsmugleaf/tripwire/Masks.java
@@ -0,0 +1,16 @@
+package com.github.drsmugleaf.tripwire;
+
+/**
+ * Created by DrSmugleaf on 16/05/2018.
+ */
+public enum Masks {
+
+    PANDEMIC_HORDE(4537.0);
+
+    public final double ID;
+
+    Masks(double id) {
+        ID = id;
+    }
+
+}
