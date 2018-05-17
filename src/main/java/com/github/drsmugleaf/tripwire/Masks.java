3366d02ef52f05fe239beff0deb1818a8ff57bc1
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-17 13:10:01 +0200

    Added name field in Masks

--- src/main/java/com/github/drsmugleaf/tripwire/Masks.java
+++ src/main/java/com/github/drsmugleaf/tripwire/Masks.java
@@ -1,15 +1,21 @@
 package com.github.drsmugleaf.tripwire;
 
+import javax.annotation.Nonnull;
+
 /**
  * Created by DrSmugleaf on 16/05/2018.
  */
 public enum Masks {
 
-    PANDEMIC_HORDE(4537.0);
+    PANDEMIC_HORDE_INC("Pandemic Horde Inc.", 4537.0);
+
+    @Nonnull
+    public final String NAME;
 
     public final double ID;
 
-    Masks(double id) {
+    Masks(@Nonnull String name, double id) {
+        NAME = name;
         ID = id;
     }
 
