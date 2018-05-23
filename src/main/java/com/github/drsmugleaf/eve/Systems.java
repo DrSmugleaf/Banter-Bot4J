568ae2ae19b4bb84bc4847cef986e14f6c06fac0
DrSmugleaf <javier.frayluis@gmail.com>
2018-05-24 01:17:52 +0200

    Split stargates.csv into stargates and systems csvs

--- src/main/java/com/github/drsmugleaf/eve/Systems.java
+++ src/main/java/com/github/drsmugleaf/eve/Systems.java
@@ -18,7 +18,10 @@ import java.util.Map;
 public class Systems {
 
     @Nonnull
-    private static final String FILE_NAME = "./src/main/java/com/github/drsmugleaf/eve/stargates.csv";
+    private static final String STARGATES_FILE_NAME = "./src/main/java/com/github/drsmugleaf/eve/stargates.csv";
+
+    @Nonnull
+    private static final String SYSTEMS_FILE_NAME = "./src/main/java/com/github/drsmugleaf/eve/systems.csv";
 
     @Nonnull
     public static final Multimap<Integer, Integer> CONNECTIONS = ArrayListMultimap.create();
@@ -27,21 +30,28 @@ public class Systems {
     public static final Map<Integer, String> NAMES = new HashMap<>();
 
     static {
-        try (CSVReader reader = new CSVReaderBuilder(new FileReader(FILE_NAME)).withSkipLines(1).build()) {
+        try (
+                CSVReader stargates = new CSVReaderBuilder(new FileReader(STARGATES_FILE_NAME)).withSkipLines(1).build();
+                CSVReader systems = new CSVReaderBuilder(new FileReader(SYSTEMS_FILE_NAME)).withSkipLines(1).build();
+        ) {
             String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
-                Integer from = Integer.parseInt(nextLine[1]);
-                NAMES.put(from, nextLine[0]);
-                Integer to = Integer.parseInt(nextLine[3]);
-                NAMES.put(to, nextLine[2]);
 
+            while ((nextLine = stargates.readNext()) != null) {
+                Integer from = Integer.parseInt(nextLine[0]);
+                Integer to = Integer.parseInt(nextLine[1]);
                 CONNECTIONS.put(from, to);
             }
+
+            while ((nextLine = systems.readNext()) != null) {
+                Integer id = Integer.parseInt(nextLine[0]);
+                String name = nextLine[1];
+                NAMES.put(id, name);
+            }
         } catch (FileNotFoundException e) {
-            EVE.LOGGER.error("File " + FILE_NAME + " doesn't exist", e);
+            EVE.LOGGER.error("File " + STARGATES_FILE_NAME + " doesn't exist", e);
             System.exit(1);
         } catch (IOException e) {
-            EVE.LOGGER.error("Error reading csv " + FILE_NAME, e);
+            EVE.LOGGER.error("Error reading csv " + STARGATES_FILE_NAME, e);
             System.exit(1);
         }
     }
