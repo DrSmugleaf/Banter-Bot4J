a33f528fa28786b2a6574feca63c7b41325bf153
DrSmugleaf <javier.frayluis@gmail.com>
2018-06-29 14:17:44 +0200

    Added method to get a player's available actions

--- src/main/java/com/github/drsmugleaf/blackjack/Actions.java
+++ src/main/java/com/github/drsmugleaf/blackjack/Actions.java
@@ -1,6 +1,7 @@
 package com.github.drsmugleaf.blackjack;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Created by DrSmugleaf on 01/05/2018.
@@ -44,6 +45,17 @@ public enum Actions {
             return player.HAND.size() == 2;
         }
     };
+
+    @Nullable
+    public static Actions getAction(@Nonnull String name) {
+        for (Actions action : values()) {
+            if (action.name().equalsIgnoreCase(name)) {
+                return action;
+            }
+        }
+
+        return null;
+    }
 
     public abstract boolean isValidFor(@Nonnull Player player);
 
--- src/main/java/com/github/drsmugleaf/blackjack/Player.java
+++ src/main/java/com/github/drsmugleaf/blackjack/Player.java
@@ -1,6 +1,8 @@
 package com.github.drsmugleaf.blackjack;
 
 import javax.annotation.Nonnull;
+import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * Created by DrSmugleaf on 01/05/2018.
@@ -27,8 +29,20 @@ public class Player {
         return action;
     }
 
-    public boolean setAction(@Nonnull Actions action) {
-        if (action.isValidFor(this)) {
+    public Set<Actions> getAvailableActions() {
+        EnumSet<Actions> actions = EnumSet.noneOf(Actions.class);
+        for (Actions action : Actions.values()) {
+            if (action.isValidFor(this)) {
+                actions.add(action);
+            }
+        }
+
+        return actions;
+    }
+
+    public boolean setAction(@Nonnull String actionName) {
+        Actions action = Actions.getAction(actionName);
+        if (action != null && status == Status.PLAYING && action.isValidFor(this)) {
             this.action = action;
             return true;
         } else {
