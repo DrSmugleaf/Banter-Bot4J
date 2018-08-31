423474329d5d06b623067d43b00bd1cb0ce03800
DrSmugleaf <javier.frayluis@gmail.com>
2018-08-31 15:02:11 +0200

    Created method to get random leaders

--- src/main/java/com/github/drsmugleaf/civilization/vi/Leaders.java
+++ src/main/java/com/github/drsmugleaf/civilization/vi/Leaders.java
@@ -1,6 +1,10 @@
 package com.github.drsmugleaf.civilization.vi;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by DrSmugleaf on 31/08/2018
@@ -49,6 +53,13 @@ public enum Leaders {
 
     Leaders(@Nonnull String name) {
         NAME = name;
+    }
+
+    @Nonnull
+    public List<Leaders> random(int amount) {
+        List<Leaders> leaders = new ArrayList<>(Arrays.asList(values()));
+        Collections.shuffle(leaders);
+        return leaders.subList(0, amount - 1);
     }
 
 }
