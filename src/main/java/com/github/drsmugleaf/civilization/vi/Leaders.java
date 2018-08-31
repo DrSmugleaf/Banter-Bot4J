209f903afe190670dc2f29c9b0bc73178e8ebb69
DrSmugleaf <javier.frayluis@gmail.com>
2018-08-31 14:52:50 +0200

    Created Civilization VI Leaders enum

---
+++ src/main/java/com/github/drsmugleaf/civilization/vi/Leaders.java
@@ -0,0 +1,54 @@
+package com.github.drsmugleaf.civilization.vi;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by DrSmugleaf on 31/08/2018
+ */
+public enum Leaders {
+
+    ALEXANDER("Alexander"),
+    AMANITORE("Amanitore"),
+    CATHERINE_DE_MEDICI("Catherine de Medici"),
+    CHANDRAGUPTA("Chandragupta"),
+    CLEOPATRA("Cleopatra"),
+    CYRUS("Cyrus"),
+    FREDERICK_BARBAROSSA("Frederick Barbarossa"),
+    GANDHI("Gandhi"),
+    GENGHIS_KHAN("Genghis Khan"),
+    GILGAMESH("Gilgamesh"),
+    GITARJA("Gitarja"),
+    GORGO("Gorgo"),
+    HARALD_HARDRADA("Harald Hardrada"),
+    HOJO_TOKIMUNE("Hojo Tokimune"),
+    JADWIGA("Jadwiga"),
+    JAYAVARMAN_VII("Jayavarman VII"),
+    JOHN_CURTIN("John Curtin"),
+    LAUTARO("Lautaro"),
+    MONTEZUMA("Montezuma"),
+    MVEMBA_A_NZINGA("Mvemba a Nzinga"),
+    PEDRO_II("Pedro II"),
+    PERICLES("Pericles"),
+    PETER("Peter"),
+    PHILIP_II("Philip II"),
+    POUNDMAKER("Poundmaker"),
+    QIN_SHI_HUANG("Qin Shi Huang"),
+    ROBERT_THE_BRUCE("Robert the Bruce"),
+    SALADIN("Saladin"),
+    SEONDEOK("Seondeok"),
+    SHAKA("Shaka"),
+    TAMAR("Tamar"),
+    TEDDY_ROOSEVELT("Teddy Roosevelt"),
+    TOMYRIS("Tomyris"),
+    TRAJAN("Trajan"),
+    VICTORIA("Victoria"),
+    WILHELMINA("Wilhelmina");
+
+    @Nonnull
+    public final String NAME;
+
+    Leaders(@Nonnull String name) {
+        NAME = name;
+    }
+
+}
