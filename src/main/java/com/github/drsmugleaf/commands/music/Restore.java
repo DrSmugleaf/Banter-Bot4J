e4a3ed6acb16c234fcc5d3543a67727cd28be7fa
DrSmugleaf <javier.frayluis@gmail.com>
2018-11-04 12:49:36 +0100

    Changed undostop command name to restore
    Added undostop as an alias

--- src/main/java/com/github/drsmugleaf/commands/music/Restore.java
+++ src/main/java/com/github/drsmugleaf/commands/music/Restore.java
@@ -6,7 +6,6 @@ import com.github.drsmugleaf.commands.api.CommandReceivedEvent;
 import com.github.drsmugleaf.commands.api.tags.Tags;
 import com.github.drsmugleaf.music.TrackScheduler;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
 
@@ -17,32 +16,31 @@ import java.util.List;
 /**
  * Created by DrSmugleaf on 09/06/2018
  */
-@CommandInfo(tags = {Tags.GUILD_ONLY, Tags.DELETE_COMMAND_MESSAGE})
-public class Undostop extends MusicCommand {
+@CommandInfo(aliases = {"undostop"}, tags = {Tags.GUILD_ONLY, Tags.DELETE_COMMAND_MESSAGE})
+public class Restore extends MusicCommand {
 
-    protected Undostop(@Nonnull CommandReceivedEvent event, @Nonnull Arguments args) {
+    protected Restore(@Nonnull CommandReceivedEvent event, @Nonnull Arguments args) {
         super(event, args);
     }
 
     @Override
     public void run() {
         IGuild guild = EVENT.getGuild();
-        IChannel channel = EVENT.getChannel();
-
         IUser author = EVENT.getAuthor();
         AbstractMap.SimpleEntry<IGuild, IUser> pair = new AbstractMap.SimpleEntry<>(guild, author);
         List<AudioTrack> tracks = Music.UNDO_STOP_CACHE.getIfPresent(pair);
+
         if (tracks == null) {
             EVENT.reply("You haven't stopped any tracks in the last minute.");
             return;
         }
 
         Music.UNDO_STOP_CACHE.invalidate(pair);
-
         TrackScheduler scheduler = Music.getGuildMusicManager(guild).getScheduler();
         tracks.addAll(scheduler.cloneTracks());
         scheduler.stop();
         scheduler.queue(tracks);
+
         EVENT.reply("Restored all stopped tracks.");
     }
 
