ff38a03b4bb0e118573b7d5419453b2cb6d81677
DrSmugleaf <javier.frayluis@gmail.com>
2018-11-06 13:41:28 +0100

    Created KillersAPI class and method to get killer data

---
+++ src/main/java/com/github/drsmugleaf/deadbydaylight/dennisreep/KillersAPI.java
@@ -0,0 +1,25 @@
+package com.github.drsmugleaf.deadbydaylight.dennisreep;
+
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by DrSmugleaf on 06/11/2018
+ */
+public class KillersAPI extends API {
+
+    private static final String KILLER_DATA_ENDPOINT = "getKillerData/";
+
+    private KillersAPI() {}
+
+    @Nonnull
+    public static List<Killer> getKillerData() {
+        JsonArray json = getResponse(KILLER_DATA_ENDPOINT).get("Killers").getAsJsonArray();
+        return GSON.fromJson(json, new TypeToken<ArrayList<Killer>>(){}.getType());
+    }
+
+}
