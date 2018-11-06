ff957437bdd172909265282ffae4c0c248df72c0
DrSmugleaf <javier.frayluis@gmail.com>
2018-11-06 22:31:53 +0100

    Added color property to Tiers enum

--- src/main/java/com/github/drsmugleaf/deadbydaylight/Tiers.java
+++ src/main/java/com/github/drsmugleaf/deadbydaylight/Tiers.java
@@ -1,23 +1,28 @@
 package com.github.drsmugleaf.deadbydaylight;
 
 import javax.annotation.Nonnull;
+import java.awt.*;
 
 /**
  * Created by DrSmugleaf on 06/11/2018
  */
 public enum Tiers {
 
-    S("S"),
-    A("A"),
-    B("B"),
-    C("C"),
-    D("D");
+    S("S", new Color(60, 0, 60, 127)),
+    A("A", new Color(0, 60, 0, 127)),
+    B("B", new Color(0, 60, 60, 127)),
+    C("C", new Color(60, 60, 0, 127)),
+    D("D", new Color(60, 0, 0, 127));
 
     @Nonnull
     public final String NAME;
 
-    Tiers(@Nonnull String name) {
+    @Nonnull
+    public final Color COLOR;
+
+    Tiers(@Nonnull String name, @Nonnull Color color) {
         NAME = name;
+        COLOR = color;
     }
 
     @Nonnull
