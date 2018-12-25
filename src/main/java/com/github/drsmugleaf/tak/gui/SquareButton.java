f4b883a0f4b7f9db2a2689b93f2d85827320dc05
DrSmugleaf <javier.frayluis@gmail.com>
2018-12-25 22:30:04 +0100

    Modified SquareButton.update method to stack pieces as smaller images

--- src/main/java/com/github/drsmugleaf/tak/gui/SquareButton.java
+++ src/main/java/com/github/drsmugleaf/tak/gui/SquareButton.java
@@ -8,6 +8,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 /**
  * Created by DrSmugleaf on 25/12/2018
@@ -67,13 +68,26 @@ public class SquareButton {
 
     @NotNull
     protected JButton update(@NotNull Square square) {
+        List<Piece> pieces = square.getPieces();
         Image background = getDefaultImage().getImage();
+        Graphics g = background.getGraphics();
+        int backgroundHeight = background.getHeight(null);
+        int backgroundWidth = background.getWidth(null);
 
-        for (Piece piece : square.getPieces()) {
-            Graphics g = background.getGraphics();
-            g.drawImage(piece.toImage(SIZE, SIZE), 0, 0, null);
-            g.dispose();
+        for (int i = 0; i < pieces.size(); i++) {
+            Piece piece = pieces.get(i);
+            int pieceSize = SIZE - 20 * i;
+            Image image = piece.toImage(pieceSize, pieceSize);
+
+            g.drawImage(
+                    image,
+                    (backgroundWidth - image.getWidth(null)) / 2,
+                    (backgroundHeight - image.getHeight(null)) / 2,
+                    null
+            );
         }
+
+        g.dispose();
 
         BUTTON.setIcon(new ImageIcon(background));
 
