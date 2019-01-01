416f577162846c4ab33ccf0982730ed4729036da
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-01 20:40:35 +0100

    Added placing standing and capstones as a GuiPlayer

--- src/main/java/com/github/drsmugleaf/tak/gui/GuiPlayer.java
+++ src/main/java/com/github/drsmugleaf/tak/gui/GuiPlayer.java
@@ -9,6 +9,10 @@ import com.github.drsmugleaf.tak.player.Player;
 import com.github.drsmugleaf.tak.player.PlayerInformation;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 /**
  * Created by DrSmugleaf on 31/12/2018
  */
@@ -29,18 +33,67 @@ public class GuiPlayer extends Player {
         GAME = (GuiGame) game;
 
         SquareButton[][] pieces = GAME.BOARD_PANEL.pieces;
+        GuiPlayer player = this;
         for (int rowIndex = 0; rowIndex < pieces.length; rowIndex++) {
             SquareButton[] row = pieces[rowIndex];
             for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
                 SquareButton square = row[columnIndex];
                 int finalRowIndex = rowIndex;
                 int finalColumnIndex = columnIndex;
-                square.getButton().addActionListener(e -> {
+
+                JButton button = square.getButton();
+                button.addActionListener(e -> {
                     if (game.getNextPlayer() == this) {
                         NEXT_MOVE = new Coordinates(finalRowIndex, finalColumnIndex, Type.FLAT_STONE);
-                        synchronized (this) {
-                            this.notify();
+                        if (NEXT_MOVE.canPlace(this)) {
+                            synchronized (this) {
+                                this.notify();
+                            }
                         }
+                    }
+                });
+
+                button.addMouseListener(new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                    }
+
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        button.getModel().setArmed(true);
+                        button.getModel().setPressed(true);
+
+                        Type type;
+                        if (e.getButton() == MouseEvent.BUTTON3) {
+                            type = Type.STANDING_STONE;
+                        } else if (e.getButton() == MouseEvent.BUTTON2) {
+                            type = Type.CAPSTONE;
+                        } else {
+                            return;
+                        }
+
+                        NEXT_MOVE = new Coordinates(finalRowIndex, finalColumnIndex, type);
+                        if (NEXT_MOVE.canPlace(player)) {
+                            synchronized (player) {
+                                player.notify();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        button.getModel().setArmed(false);
+                        button.getModel().setPressed(false);
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+
                     }
                 });
             }
