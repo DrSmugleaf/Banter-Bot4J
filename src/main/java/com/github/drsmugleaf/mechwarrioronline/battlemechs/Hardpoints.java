81754d1e51cb0c6b93043879051608fbad24fa7e
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-10 22:25:44 +0100

    Created hardpoints enum

---
+++ src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Hardpoints.java
@@ -0,0 +1,14 @@
+package com.github.drsmugleaf.mechwarrioronline.battlemechs;
+
+/**
+ * Created by DrSmugleaf on 10/01/2019
+ */
+public enum Hardpoints {
+
+    AMS,
+    BALLISTIC,
+    ECM,
+    ENERGY,
+    MISSILE
+
+}
