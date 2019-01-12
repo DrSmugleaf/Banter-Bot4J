c19593eff0a175cc94d0e29e1e33de63c966468b
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-12 14:54:53 +0100

    Added abbreviation field to hardpoints

--- src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Hardpoints.java
+++ src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Hardpoints.java
@@ -1,14 +1,39 @@
 package com.github.drsmugleaf.mechwarrioronline.battlemechs;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by DrSmugleaf on 10/01/2019
  */
 public enum Hardpoints {
 
-    AMS,
-    BALLISTIC,
-    ECM,
-    ENERGY,
-    MISSILE
+    AMS("AMS"),
+    BALLISTIC("B"),
+    ECM("ECM"),
+    ENERGY("E"),
+    MISSILE("M");
+
+    @NotNull
+    private final String ABBREVIATION;
+
+    Hardpoints(@NotNull String abbreviation) {
+        ABBREVIATION = abbreviation;
+    }
+
+    @NotNull
+    public static Hardpoints from(@NotNull String abbreviation) {
+        for (Hardpoints hardpoint : values()) {
+            if (hardpoint.ABBREVIATION.equals(abbreviation)) {
+                return hardpoint;
+            }
+        }
+
+        throw new IllegalArgumentException("No hardpoint found with abbreviation " + abbreviation);
+    }
+
+    @NotNull
+    public String getAbbreviation() {
+        return ABBREVIATION;
+    }
 
 }
