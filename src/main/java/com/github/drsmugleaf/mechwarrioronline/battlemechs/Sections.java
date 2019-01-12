299a8c272c6bc6b6408f7577d5415e19f9a0ddbc
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-12 14:55:27 +0100

    Added name field to Sections

--- src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Sections.java
+++ src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Sections.java
@@ -1,17 +1,31 @@
 package com.github.drsmugleaf.mechwarrioronline.battlemechs;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by DrSmugleaf on 12/01/2019
  */
 public enum Sections {
 
-    CENTER_TORSO,
-    HEAD,
-    LEFT_ARM,
-    LEFT_LEG,
-    LEFT_TORSO,
-    RIGHT_ARM,
-    RIGHT_LEG,
-    RIGHT_TORSO
+    CENTER_TORSO("Center"),
+    HEAD("Head"),
+    LEFT_ARM("Left Arm"),
+    LEFT_LEG("Left Leg"),
+    LEFT_TORSO("Left Torso"),
+    RIGHT_ARM("Right Arm"),
+    RIGHT_LEG("Right Leg"),
+    RIGHT_TORSO("Right Torso");
+
+    @NotNull
+    private final String NAME;
+
+    Sections(@NotNull String name) {
+        NAME = name;
+    }
+
+    @NotNull
+    public String getName() {
+        return NAME;
+    }
 
 }
