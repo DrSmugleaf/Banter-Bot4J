d93aa1d3b2e8b0424014cec82cefa0d30d1054f5
DrSmugleaf <javier.frayluis@gmail.com>
2019-01-12 02:26:45 +0100

    Created battlemechs Sections enum

---
+++ src/main/java/com/github/drsmugleaf/mechwarrioronline/battlemechs/Sections.java
@@ -0,0 +1,17 @@
+package com.github.drsmugleaf.mechwarrioronline.battlemechs;
+
+/**
+ * Created by DrSmugleaf on 12/01/2019
+ */
+public enum Sections {
+
+    CENTER_TORSO,
+    HEAD,
+    LEFT_ARM,
+    LEFT_LEG,
+    LEFT_TORSO,
+    RIGHT_ARM,
+    RIGHT_LEG,
+    RIGHT_TORSO
+
+}
