4c14c77d8d3c1dc8a8a830d0b2f955841e5e9b89
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2019-11-17 20:30:19 +0100

    Overrided getSpecies and getAbilities in IPokemonIII

--- src/main/java/com/github/drsmugleaf/pokemon2/generations/iii/pokemon/IPokemonIII.java
+++ src/main/java/com/github/drsmugleaf/pokemon2/generations/iii/pokemon/IPokemonIII.java
@@ -1,13 +1,22 @@
 package com.github.drsmugleaf.pokemon2.generations.iii.pokemon;
 
 import com.github.drsmugleaf.pokemon2.base.pokemon.IPokemon;
+import com.github.drsmugleaf.pokemon2.generations.iii.pokemon.ability.IAbility;
+import com.github.drsmugleaf.pokemon2.generations.iii.pokemon.species.ISpeciesIII;
 import com.github.drsmugleaf.pokemon2.generations.iii.pokemon.stat.nature.INature;
+import com.google.common.collect.ImmutableSet;
 
 /**
  * Created by DrSmugleaf on 15/11/2019
  */
-public interface IPokemonIII<T extends IPokemonIII<T>> extends IPokemon<T> {
+public interface IPokemonIII<T extends IPokemonIII<T>> extends IPokemon<T>, ISpeciesIII<T> {
 
+    @Override
+    ISpeciesIII<T> getSpecies();
     INature getNature();
+    @Override
+    default ImmutableSet<IAbility> getAbilities() {
+        return getSpecies().getAbilities();
+    }
 
 }
