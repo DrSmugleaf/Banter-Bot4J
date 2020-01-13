5e6f8b32fc62c2e7299902fbb293165866807f88
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-01-13 13:39:43 +0100

    Created DLBot

---
+++ src/main/java/com/github/drsmugleaf/tak/bot/neural/deeplearning/DLBot.java
@@ -0,0 +1,42 @@
+package com.github.drsmugleaf.tak.bot.neural.deeplearning;
+
+import com.github.drsmugleaf.tak.board.ICoordinates;
+import com.github.drsmugleaf.tak.bot.Bot;
+import com.github.drsmugleaf.tak.game.IGame;
+import com.github.drsmugleaf.tak.pieces.IColor;
+import com.github.drsmugleaf.tak.player.IPlayer;
+import com.github.drsmugleaf.tak.player.IPlayerInformation;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+import java.util.function.Function;
+
+/**
+ * Created by DrSmugleaf on 12/01/2020
+ */
+public class DLBot extends Bot {
+
+    private final MultiLayerNetwork NETWORK;
+
+    protected DLBot(String name, IGame game, IColor color, MultiLayerNetwork network) {
+        super(name, game, color, false);
+        NETWORK = network;
+    }
+
+    public static IPlayer from(IPlayerInformation information, MultiLayerNetwork network) {
+        return new DLBot(information.getName(), information.getGame(), information.getColor(), network);
+    }
+
+    public static Function<IPlayerInformation, IPlayer> from(MultiLayerNetwork network) {
+        return information -> from(information, network);
+    }
+
+    public MultiLayerNetwork getNetwork() {
+        return NETWORK;
+    }
+
+    @Override
+    public ICoordinates getNextAction() {
+        return null;
+    }
+
+}
