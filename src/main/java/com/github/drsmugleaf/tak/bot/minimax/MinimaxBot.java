50108abcbe2afa22e9ceb1c16646e390a9fbceae
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-01-17 03:03:31 +0100

    Changed MinimaxBot to invert the score at the end

--- src/main/java/com/github/drsmugleaf/tak/bot/minimax/MinimaxBot.java
+++ src/main/java/com/github/drsmugleaf/tak/bot/minimax/MinimaxBot.java
@@ -57,12 +57,12 @@ public class MinimaxBot extends Bot {
         return board.getRoad() != null;
     }
 
-    private int getScore(IBoard board) {
+    protected int getScore(IBoard board, IColor color) {
         IColor winner = board.getRoad();
         if (winner != null) {
-            if (winner == getColor()) {
+            if (winner == color) {
                 return Integer.MAX_VALUE;
-            } else if (winner == getColor().getOpposite()) {
+            } else if (winner == color.getOpposite()) {
                 return Integer.MIN_VALUE;
             } else {
                 throw new IllegalArgumentException("Unrecognized color: " + winner);
@@ -81,10 +81,10 @@ public class MinimaxBot extends Bot {
                 int adjacentScore = board.getAdjacent(square).getConnections().size() * 10;
 
                 IColor topColor = topPiece.getColor();
-                if (topColor == getColor()) {
+                if (topColor == color) {
                     score++;
                     score += adjacentScore;
-                } else if (topColor == getColor().getOpposite()) {
+                } else if (topColor == color.getOpposite()) {
                     score--;
                     score -= adjacentScore;
                 } else {
@@ -96,34 +96,35 @@ public class MinimaxBot extends Bot {
         return score;
     }
 
-    protected final Pair<IAction, Integer> getMax(List<IAction> actions, IBoard board, IColor nextPlayer, int alpha, int beta, final int depth) {
+    protected final Pair<IAction, Integer> getMax(List<IAction> actions, IBoard board, IColor nextColor, int alpha, int beta, final int depth) {
         IAction bestAction = null;
         int score = 0;
 
         for (IAction action : actions) {
             int finalAlpha = alpha;
             int finalBeta = beta;
-            score = action.with(board, nextPlayer, copy -> {
+            score = action.with(board, nextColor, copy -> {
                 if (depth > 1 && !isTerminal(copy)) {
-                    List<IAction> availableActions = getGame().getPlayer(nextPlayer.getOpposite()).getAvailableActions(copy);
-                    return getMax(availableActions, copy, nextPlayer.getOpposite(), finalAlpha, finalBeta, depth - 1).getValue();
+                    List<IAction> availableActions = getGame().getOtherPlayer(this).getAvailableActions(copy);
+                    return getMax(availableActions, copy, nextColor.getOpposite(), finalAlpha, finalBeta, depth - 1).getValue();
                 } else {
-                    return getScore(copy);
+                    int tempScore = getScore(copy, nextColor);
+                    return nextColor == getColor() ? tempScore : -tempScore;
                 }
             }) - depth;
 
-            if (nextPlayer == getColor()) {
+            if (nextColor == getColor()) {
                 if (score > alpha) {
                     alpha = score;
                     bestAction = action;
                 }
-            } else if (nextPlayer.getOpposite() == getColor()) {
+            } else if (nextColor.getOpposite() == getColor()) {
                 if (score < beta) {
                     beta = score;
                     bestAction = action;
                 }
             } else {
-                throw new IllegalArgumentException("Unrecognized color: " + nextPlayer);
+                throw new IllegalArgumentException("Unrecognized color: " + nextColor);
             }
 
             if (alpha >= beta) {
