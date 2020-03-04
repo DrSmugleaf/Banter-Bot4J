b8598e74377cba9e77f7060ef64d32427a9e6d64
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-03-04 16:02:24 +0100

    Added missing TransformerSet.of methods with validators

--- src/main/java/com/github/drsmugleaf/commands/api/converter/TransformerSet.java
+++ src/main/java/com/github/drsmugleaf/commands/api/converter/TransformerSet.java
@@ -206,6 +206,168 @@ public class TransformerSet {
         );
     }
 
+    public static <E1, E2> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2)
+        );
+    }
+
+    public static <E1, E2, E3> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3)
+        );
+    }
+
+    public static <E1, E2, E3, E4> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5, E6> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5,
+            Class<E6> type6, ITransformer<E6> transformer6, IValidator<E6> validator6
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5),
+                new Transformer<>(type6, transformer6, validator6)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5, E6, E7> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5,
+            Class<E6> type6, ITransformer<E6> transformer6, IValidator<E6> validator6,
+            Class<E7> type7, ITransformer<E7> transformer7, IValidator<E7> validator7
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5),
+                new Transformer<>(type6, transformer6, validator6),
+                new Transformer<>(type7, transformer7, validator7)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5, E6, E7, E8> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5,
+            Class<E6> type6, ITransformer<E6> transformer6, IValidator<E6> validator6,
+            Class<E7> type7, ITransformer<E7> transformer7, IValidator<E7> validator7,
+            Class<E8> type8, ITransformer<E8> transformer8, IValidator<E8> validator8
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5),
+                new Transformer<>(type6, transformer6, validator6),
+                new Transformer<>(type7, transformer7, validator7),
+                new Transformer<>(type8, transformer8, validator8)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5, E6, E7, E8, E9> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5,
+            Class<E6> type6, ITransformer<E6> transformer6, IValidator<E6> validator6,
+            Class<E7> type7, ITransformer<E7> transformer7, IValidator<E7> validator7,
+            Class<E8> type8, ITransformer<E8> transformer8, IValidator<E8> validator8,
+            Class<E9> type9, ITransformer<E9> transformer9, IValidator<E9> validator9
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5),
+                new Transformer<>(type6, transformer6, validator6),
+                new Transformer<>(type7, transformer7, validator7),
+                new Transformer<>(type8, transformer8, validator8),
+                new Transformer<>(type9, transformer9, validator9)
+        );
+    }
+
+    public static <E1, E2, E3, E4, E5, E6, E7, E8, E9, E10> TransformerSet of(
+            Class<E1> type1, ITransformer<E1> transformer1, IValidator<E1> validator1,
+            Class<E2> type2, ITransformer<E2> transformer2, IValidator<E2> validator2,
+            Class<E3> type3, ITransformer<E3> transformer3, IValidator<E3> validator3,
+            Class<E4> type4, ITransformer<E4> transformer4, IValidator<E4> validator4,
+            Class<E5> type5, ITransformer<E5> transformer5, IValidator<E5> validator5,
+            Class<E6> type6, ITransformer<E6> transformer6, IValidator<E6> validator6,
+            Class<E7> type7, ITransformer<E7> transformer7, IValidator<E7> validator7,
+            Class<E8> type8, ITransformer<E8> transformer8, IValidator<E8> validator8,
+            Class<E9> type9, ITransformer<E9> transformer9, IValidator<E9> validator9,
+            Class<E10> type10, ITransformer<E10> transformer10, IValidator<E10> validator10
+    ) {
+        return of(
+                new Transformer<>(type1, transformer1, validator1),
+                new Transformer<>(type2, transformer2, validator2),
+                new Transformer<>(type3, transformer3, validator3),
+                new Transformer<>(type4, transformer4, validator4),
+                new Transformer<>(type5, transformer5, validator5),
+                new Transformer<>(type6, transformer6, validator6),
+                new Transformer<>(type7, transformer7, validator7),
+                new Transformer<>(type8, transformer8, validator8),
+                new Transformer<>(type9, transformer9, validator9),
+                new Transformer<>(type10, transformer10, validator10)
+        );
+    }
+
     public ImmutableSet<Transformer<?>> get() {
         return TRANSFORMERS;
     }
